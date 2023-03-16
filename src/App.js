bc95dd45966d9c57763c69cac795aedd1e52d7f8
rehmaund <rehmaund44@gmail.com>
2023-03-16 17:18:32 -0400

    started a7 labs

--- src/App.js
+++ src/App.js
@@ -8,10 +8,8 @@ function App() {
       <BrowserRouter>
       <div className="container">
           <Routes>
-              <Route index
+              <Route path="/*"
                      element={<Labs/>}/>
-              <Route path="/hello"
-                     element={<HelloWorld/>}/>
               <Route path="/tuiter/*"
                      element={<Tuiter/>}/>
           </Routes>
